d844e71de2218407a659a081450bd8d77b0cfa37
Константин Рудаков <kostarudakov2005@gmail.com>
2024-11-17 18:23:27 +0500

    solved problem, easy, 2 loops, maybe possible to do it with only 1 loop, but don't know how.

--- ACMP-HomeWork/ACMP-HomeWork.cpp
+++ ACMP-HomeWork/ACMP-HomeWork.cpp
@@ -5,9 +5,46 @@ using namespace std;
 
 int main()
 {
+	int N;
+	cin >> N;
+	int* arrayOfNumbers = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arrayOfNumbers[i];
+	}
+	int indexOfMax = 0, Max = INT_MIN, indexOfMin = 0, Min = INT_MAX;
+	int sumOfPositive = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (arrayOfNumbers[i] > Max)
+		{
+			indexOfMax = i;
+			Max = arrayOfNumbers[i];
+		}
+		if (arrayOfNumbers[i] < Min)
+		{
+			indexOfMin = i;
+			Min = arrayOfNumbers[i];
+		}
+		if (arrayOfNumbers[i] > 0)
+		{
+			sumOfPositive += arrayOfNumbers[i];
+		}
 
+	}
+	int productBetween = 1;
+	int start = indexOfMin, end = indexOfMax;
+	if (indexOfMax < indexOfMin)
+	{
+		start = indexOfMax;
+		end = indexOfMin;
+	}
+	for (int i = start + 1; i < end; i++)
+	{
+		productBetween *= arrayOfNumbers[i];
+	}
 
-
-
+	cout << sumOfPositive << " " << productBetween << endl;
+	
 	return 0;
 }
